5828ebdef1cac2ed09b57d2786d2f45fd7a10991
evandroitalo <evandro.italo@gmail.com>
2024-10-20 23:00:05 -0300

    Desafio de  Ruby 1 - Dio.me

---
+++ desafio1.rb
@@ -0,0 +1,15 @@
+#1)Saida de dados na tela --> aparecer um comando na tela
+# Usuário digite seu nome
+print "Digite seu nome: "
+nome = gets.chomp
+
+# Usuário digite seu sobrenome
+print "Digite seu Sobrenome: "
+sobrenome = gets.chomp
+
+# Usuário digite seu idade
+print "Digite sua idade: "
+idade = gets.chomp
+
+#3) saída final
+puts "Seu nome é #{nome} #{sobrenome} sua idade é #{idade}!"
